34aab0e2e9542fcf8d1fe0959059376db8d10dd0
BongyGit <93770520+BongyGit@users.noreply.github.com>
2026-05-29 10:14:09 +0100

    Update MainActivity to add kebab menu with backup and restore options

--- app/src/main/java/bongydev/com/bongyfilms/MainActivity.kt
+++ app/src/main/java/bongydev/com/bongyfilms/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,27 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, FilmListActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_backup -> {
+                val intent = Intent(this, BackupActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_restore -> {
+                val intent = Intent(this, RestoreActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
